da57a0daa9591656a4010b481bfb22d9d04d4600
Jaroslaw Ireneusz Lorenc <jaro0013@edu.eal.dk>
2018-10-24 11:11:18 +0200

    Add project files.

---
+++ Morgengry/Amulet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public enum Level { low, medium, high };
+    public class Amulet
+    {
+        string ItemID { get; set; }
+        string Design { get; set; }
+
+        Level Quality { get; set; }
+
+
+
+        
+        public Amulet(string itemID):
+            this(itemID, Level.medium, "")
+        {
+            //this.itemID = itemID;
+
+        }
+
+        public Amulet(string itemID, Level quality):
+            this(itemID, quality, "")
+        {
+            //this.itemID = itemID;
+            //this.quality = quality;
+        
+        }
+
+        public Amulet (string itemID,Level quality,string design)
+        {
+            this.ItemID = itemID;
+            this.Quality = quality;
+            this.Design = design;
+        }
+        
+        public string ToString()
+        {
+            return "ItemId: " + ItemID + ", " + "Quality: " + Quality + ", " + "Design: " + Design;
+        }
+
+    }
+}
---
+++ Morgengry/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public class Book
+    {
+        string ItemID { get; set; }
+        string Title { get; set; }
+        double Price { get; set; }
+
+        public Book(string itemID):
+            this(itemID, "", 0)
+        {
+            //this.itemID = itemID;
+        }
+
+        public Book(string itemID, string title):
+            this(itemID, title, 0)
+        {
+            //this.itemID = itemID;
+            //this.title = title;
+        }
+
+        public Book(string itemID, string title,double price)
+        {
+            this.ItemID = itemID;
+            this.Title = title;
+            this.Price = price;
+        }
+
+        public string ToString()
+        {
+            return "ItemId: " + ItemID + ", " + "Title: " + Title + ", " + "Price: " + Price;
+        }
+
+    }
+
+    
+}
